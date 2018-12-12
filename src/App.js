d469a9520bd4e1487d1f6fd75712f28147d00b81
Scott Rabin <scottrabin@gmail.com>
2018-12-12 13:59:26 -0800

    Show table of transactions loaded from file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import * as parse from 'csv-parse/lib/sync';
 import * as file from './util/file';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      transactions: [],
       fileError: null,
     }
   }
@@ -27,6 +29,27 @@ class App extends Component {
             <button type="submit">Process</button>
           </div>
         </form>
+
+        <table className="transactions">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Sent</th>
+              <th>Received</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.transactions.map((txn, i) => {
+              return (
+                <tr key={i}>
+                  <td>{txn.date.toLocaleString()}</td>
+                  <td>{`${txn.sent.quantity} ${txn.sent.currency}`}</td>
+                  <td>{`${txn.received.quantity} ${txn.received.currency}`}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     );
   }
@@ -36,9 +59,31 @@ class App extends Component {
     if (this.fileInput.current.files.length !== 1) {
       return;
     }
-    file.loadFile(this.fileInput.current.files[0]).then(contents => {
-      console.log(contents);
-    });
+    file.loadFile(this.fileInput.current.files[0])
+      .then(contents => {
+        console.log(contents);
+        return contents;
+      })
+      .then(contents => parse(contents, { skip_empty_lines: true }).slice(1))
+      .then(transactions => {
+        return transactions.map(([date, receivedQty, receivedCurrency, sentQty, sentCurrency]) => {
+          return {
+            date: new Date(date),
+            received: {
+              currency: receivedCurrency,
+              quantity: parseInt(receivedQty, 10),
+            },
+            sent: {
+              currency: sentCurrency,
+              quantity: parseInt(sentQty, 10),
+            },
+          };
+        });
+      })
+      .then(transactions => {
+        console.table(transactions);
+        this.setState(s => ({ ...s, transactions }));
+      });
   }
 }
 
